d0c3b6f7e97b7bf321c088f9497c08e0c84a1295
vhi_roman <vhiannenicoleroman@gmail.com>
2025-08-22 09:38:55 +0800

    Remove comments on Home page.

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -5,14 +5,11 @@ import Header from '../Header';
 const Home: React.FC = () => {
     return (
         <div>
-            {/* Background Image */}
             <section className="h-screen bg-cover bg-center relative" style={{ backgroundImage: `url(${images.VhianneRoman})` }}>
-                {/* Header */}
                 <div className="absolute top-0 left-0 w-full">
                     <Header />
                 </div>
 
-                {/* Right Panel */}
                 <div className="absolute top-0 right-0 h-full w-full md:w-1/2 flex items-center bg-black bg-opacity-80 text-white shadow-lg border-l-4 border-blue-500">
                     <div className="p-12 w-full">
                         <p className="text-3xl md:text-6xl font-semibold leading-none tracking-tighter font-serif">Vhianne Nicole Roman</p>
